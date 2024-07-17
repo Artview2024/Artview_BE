2606f928be665c18d3eb3ea39f1b34d9ce5cdf81
emilywin825 <emilywin825@naver.com>
2024-07-18 01:38:19 +0900

    [feat]VisitedExhibitions 엔티티 생성

---
+++ src/main/java/com/backend/Artview/domain/MyReviews/domain/VisitedExhibitions.java
@@ -0,0 +1,30 @@
+package com.backend.Artview.domain.MyReviews.domain;
+
+import com.backend.Artview.domain.users.domain.Users;
+import com.backend.Artview.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "VisitedExhibitions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VisitedExhibitions extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "visited_exhibitions_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "VisitedExhibitions")
+    private List<MyReviews> myReviews = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+}
