bc9d2e1bd42eaaa1a5eab0e5c6a463bdee516f40
emilywin825 <emilywin825@naver.com>
2024-07-20 02:55:00 +0900

    [feat]내 전시기록 상세보기 기능 추가

--- src/main/java/com/backend/Artview/domain/MyReviews/dto/response/DetailMyReviewsResponseDto.java
+++ src/main/java/com/backend/Artview/domain/MyReviews/dto/response/DetailMyReviewsResponseDto.java
@@ -1,20 +1,32 @@
 package com.backend.Artview.domain.MyReviews.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.backend.Artview.domain.MyReviews.domain.MyReviews;
+import com.backend.Artview.domain.MyReviews.domain.MyReviewsContents;
+import lombok.Builder;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Builder
 public record DetailMyReviewsResponseDto(
-        Long id,
-        String name,
-        String date,
-        String gallery,
-        String MainImage,
-        String rating,
-        List<ArtList> artList
+        Long id, //ReviewId
+        String name,//전시회 명
+        String date,//방문 나라짜
+        String gallery,//갤러리 이름
+        String mainImage,
+        String rating,//별점
+        List<ArtList> artList//내용
 ) {
 
-
-
+    public static DetailMyReviewsResponseDto of(MyReviews myReview,List<MyReviewsContents> myReviewsContents) {
+        return DetailMyReviewsResponseDto.builder()
+                .id(myReview.getId())
+                .name(myReview.getExhibitionsTitle())
+                .date(myReview.getVisitedDate())
+                .gallery(myReview.getExhibitionsLocation())
+                .mainImage(myReview.getMainImageUrl())
+                .rating(Float.toString(myReview.getGrade()))
+                .artList(myReviewsContents.stream().map(v->ArtList.of(v)).collect(Collectors.toList()))
+                .build();
+    }
 }
--- src/main/java/com/backend/Artview/domain/MyReviews/service/MyReviewsServiceImpl.java
+++ src/main/java/com/backend/Artview/domain/MyReviews/service/MyReviewsServiceImpl.java
@@ -1,16 +1,19 @@
 package com.backend.Artview.domain.MyReviews.service;
 
 
-import com.backend.Artview.domain.MyReviews.domain.MyExhibitionImages;
 import com.backend.Artview.domain.MyReviews.domain.MyReviews;
 import com.backend.Artview.domain.MyReviews.dto.response.AllMyReviewsResponseDto;
 import com.backend.Artview.domain.MyReviews.dto.response.DetailMyReviewsResponseDto;
+import com.backend.Artview.domain.MyReviews.exception.MyReviewsException;
 import com.backend.Artview.domain.MyReviews.repository.MyReviewsRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.backend.Artview.domain.MyReviews.exception.MyReviewsErrorCode.MY_REVIEWS_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -19,21 +22,24 @@ import java.util.stream.Collectors;
     private final MyReviewsRepository myReviewsRepository;
 
     @Override
+    @Transactional
     public List<AllMyReviewsResponseDto> findAllMyReviews(Long userId) {
         List<MyReviews> allMyReviewsFromRepository = findAllMyReviewsFromRepository(userId);
         return allMyReviewsFromRepository.stream().map(v -> AllMyReviewsResponseDto.of(v)).collect(Collectors.toList());
     }
 
     @Override
+    @Transactional
     public DetailMyReviewsResponseDto findDetailMyReviews(Long reviewsId) {
-        return null;
+        MyReviews myReview = findDetailMyReviewsByReviewsId(reviewsId);
+        return DetailMyReviewsResponseDto.of(myReview,myReview.getMyReviewsContents());
+    }
+
+    public MyReviews findDetailMyReviewsByReviewsId(Long reviewsId){
+        return myReviewsRepository.findById(reviewsId).orElseThrow(()->new MyReviewsException(MY_REVIEWS_NOT_FOUND));
     }
 
     public List<MyReviews> findAllMyReviewsFromRepository(Long userId) {
         return myReviewsRepository.findAllByUsersId(userId);
-    }
-
-    public List<MyExhibitionImages> findMyExhibitionImages(MyReviews myReviews) {
-        return myReviews.getMyExhibitionImages();
     }
 }
