8c79ed9d95ef85ee143678a81d7a9a2dda8a0f6e
leeminjeong <emilywin825@naver.com>
2024-08-22 02:24:25 +0900

    [feat]USER_MY_REVIEWS_NOT_FOUND 에러코드 추가

--- src/main/java/com/backend/Artview/domain/myReviews/exception/MyReviewsErrorCode.java
+++ src/main/java/com/backend/Artview/domain/myReviews/exception/MyReviewsErrorCode.java
@@ -1,4 +1,4 @@
-package com.backend.Artview.domain.MyReviews.exception;
+package com.backend.Artview.domain.myReviews.exception;
 
 import com.backend.Artview.global.code.BaseErrorCode;
 import lombok.Getter;
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MyReviewsErrorCode implements BaseErrorCode {
 
-    MY_REVIEWS_NOT_FOUND(HttpStatus.NOT_FOUND,404,"ReviewId에 해당하는 리뷰를 찾을 수 없습니다.");
+    MY_REVIEWS_NOT_FOUND(HttpStatus.NOT_FOUND,404,"ReviewId에 해당하는 리뷰를 찾을 수 없습니다."),
+    USER_MY_REVIEWS_NOT_FOUND(HttpStatus.NOT_FOUND,404,"사용자가 작성한 리뷰 중 해당 ReviewId에 해당하는 리뷰를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final int statusCode;
