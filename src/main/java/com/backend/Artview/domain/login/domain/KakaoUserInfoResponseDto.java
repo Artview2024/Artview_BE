ca414ce6771877204cb01106a01215ddcc9af639
emilywin825 <emilywin825@naver.com>
2024-09-27 18:46:30 +0900

    [feat]카카오 유저 정보 받아오기 위한 KakaoUserInfoResponseDto 생성

---
+++ src/main/java/com/backend/Artview/domain/login/domain/KakaoUserInfoResponseDto.java
@@ -0,0 +1,22 @@
+package com.backend.Artview.domain.login.domain;
+
+import lombok.Data;
+
+@Data
+public class KakaoUserInfoResponseDto {
+
+    private Long id;
+    private KakaoAccount kakao_account;
+
+    @Data
+    public static class KakaoAccount {
+        private Profile profile;
+        private String email;
+    }
+
+    @Data
+    public static class Profile {
+        private String nickname;
+        private String profile_image_url;
+    }
+}
