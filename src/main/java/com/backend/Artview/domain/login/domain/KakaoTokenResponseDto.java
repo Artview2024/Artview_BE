974a3476c39f39659276d5ecef9b1e552c9d25c1
emilywin825 <emilywin825@naver.com>
2024-09-27 18:46:07 +0900

    [feat]KakaoTokenResponseDto 생성

---
+++ src/main/java/com/backend/Artview/domain/login/domain/KakaoTokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.backend.Artview.domain.login.domain;
+
+import lombok.Data;
+
+@Data
+public class KakaoTokenResponseDto {
+    private String token_type;
+    private String access_token;
+    private int expires_in; //액세스 토큰 만료 시간(초)
+    private String refresh_token; //리프레시 토큰 만료 시간(초)
+    private int refresh_token_expires_in;
+    private String scope; //인증된 사용자의 정보 조회 권한 범위
+}
