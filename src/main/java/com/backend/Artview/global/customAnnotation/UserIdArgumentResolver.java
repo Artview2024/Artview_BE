858056a0e637dc30f92011d95214d7ca98bc7c48
emilywin825 <emilywin825@naver.com>
2024-10-16 20:27:50 +0900

    [feat]JwtToken에서 UserId 추출하는 로직 @UserId라는 이름의 어노테이션으로 커스텀

---
+++ src/main/java/com/backend/Artview/global/customAnnotation/UserIdArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.backend.Artview.global.customAnnotation;
+
+import com.backend.Artview.domain.users.domain.Users;
+import com.backend.Artview.global.jwt.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasUserIdAnnotation = parameter.hasParameterAnnotation(UserId.class);
+        boolean isLongType = Long.class.isAssignableFrom(parameter.getParameterType());
+        return hasUserIdAnnotation && isLongType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String authorizationHeader = request.getHeader("Authorization");
+        String accessToken = jwtProvider.getTokenFromHeader(authorizationHeader);
+        return jwtProvider.getUserId(accessToken);
+    }
+}
