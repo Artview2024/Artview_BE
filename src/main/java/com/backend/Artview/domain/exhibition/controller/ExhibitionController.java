6040eda2f2d770a9733a7016952e81ded60392bb
emilywin825 <emilywin825@naver.com>
2024-10-28 16:01:08 +0900

    [remove] 사용하지 않는 전시 관련 컨트롤러 삭제

--- src/main/java/com/backend/Artview/domain/exhibition/controller/ExhibitionController.java
+++ src/main/java/com/backend/Artview/domain/exhibition/controller/ExhibitionController.java
@@ -17,33 +17,4 @@ import java.net.*;
 @RequestMapping("/api/exhibition")
 public class ExhibitionController {
 
-    @GetMapping
-    public ResponseEntity<String> getExhibition() throws IOException {
-        String serviceKey = "0FkN8IDSwQCddztVJ984YIyMq1TkN7Rg+/NrjtHO98aml2zS8mydFqMblg4zrWI7AAJ3B4fIBRlGSvh1SdgI3A==";
-        String serviceKey_encoded = URLEncoder.encode(serviceKey.toString(),"UTF-8");
-
-        String requestUrl = "http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period?serviceKey=";
-
-        StringBuilder url = new StringBuilder(requestUrl);
-        url.append("?").append("serviceKey").append("=").append(serviceKey_encoded);
-        url.append("&").append("solYear").append("=").append(1);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Accept", "*/*;q=0.9"); // HTTP_ERROR 방지
-        HttpEntity<String> httpRequest = new HttpEntity<>(null, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpStatus httpStatus = null;
-        try {
-            URI uri = new URI(url.toString()); // service key is not registered 오류 방지
-            return restTemplate.exchange(uri, HttpMethod.GET, httpRequest, new ParameterizedTypeReference<String>() {
-            });
-
-        } catch (UnknownContentTypeException | URISyntaxException e) {
-            throw new IOException();
-        }
-
-    }
 }
