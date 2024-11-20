70e85b01b8e58c0ee462abcb0d3fd288b4ce78bb
emilywin825 <emilywin825@naver.com>
2024-11-21 02:49:53 +0900

    [refactor] startDate,finishDate 형식 체크 함수 추가, 형식 달라서 생기는 에러 처리

--- src/main/java/com/backend/Artview/global/util/StringUtil.java
+++ src/main/java/com/backend/Artview/global/util/StringUtil.java
@@ -5,16 +5,16 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class StringUtil {
 
-    public LocalDate stringToLocalDate(String dateString){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.");
-        LocalDate otherDate=null;
+    public LocalDate stringToLocalDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        LocalDate otherDate = null;
+
         try {
             otherDate = LocalDate.parse(dateString, formatter); // String -> LocalDate 변환
         } catch (DateTimeParseException e) {
@@ -23,7 +23,11 @@ public class StringUtil {
         return otherDate;
     }
 
-    public static List<String> removeTextFromSentence(String sentence, String removeText){
+    public static boolean checkDateType(String dateString) {
+        return dateString.matches("\\d{4}\\.\\d{2}\\.\\d{2}");
+    }
+
+    public static List<String> removeTextFromSentence(String sentence, String removeText) {
         return Arrays.asList(sentence.split(removeText));
     }
 }
